ba05f7df37114064725fb5d95fc2a46b0319253c
ankim <ankim.jin@gmail.com>
2016-08-09 23:01:16 -0400

    he he

---
+++ AndyTestRoom/lang/langs.js
@@ -0,0 +1,23 @@
+var langs = {
+	'de':'deutsch',
+	'el':'ελληνικά',
+	'en':'english',
+	'es':'español',
+	'fr':'français',
+	'id':'bahasa indonesia',
+	'it':'italiano',
+	'ja':'日本語',
+	'ko':'한국어',
+	'nb':'norsk',
+	'pl':'polski',
+	'pt':'português',
+	'pt_br':'português (brasil)',
+	'ru':'русский',
+	'sv':'svenska',
+	'th':'ไทย',
+	'tr':'türkçe',
+	'uk':'українська',
+	'vi':'tiếng việt',
+	'zh_cn':'简体中文',
+	'zh_tw':'繁體中文'
+};
