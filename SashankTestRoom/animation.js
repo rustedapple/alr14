1a7bcb85099253423deeff6ead33d3ac4f6a1ae7
OrangeKyo <sashank.veligati@gmail.com>
2016-08-13 13:20:31 -0500

    Creating grid of squares. Added a couple links.

--- SashankTestRoom/animation.js
+++ SashankTestRoom/animation.js
@@ -1,8 +1,31 @@
-var animation =
-{
-   "run" : function()
+var animation = {
+   "run" : function ()
    {
       $("#div1").fadeToggle();
    },
+   "load" : function () {
+      document.getElementById("grid").innerHTML = createGrid();
+   }
 };
 
+var NUM_ROWS = 50;
+var NUM_COLUMNS = 50;
+var createGrid = function () {
+   var i, j, result = "";
+   for (i = 0; i < NUM_COLUMNS; i++) {
+      result += "<div>";
+      for (j = 0; j < NUM_ROWS; j++) {
+         result += '<div class="square" onmouseenter="onSquareEnter($(this));" onmouseleave="onSquareLeave($(this));"></div>\n';
+      }
+      result += "</div>";
+   }
+   return result;
+};
+
+var onSquareEnter = function (button) {
+   button.css('background-color', 'blue');
+};
+
+var onSquareLeave = function (button) {
+   button.css('background-color', 'red');
+};
