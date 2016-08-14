825db06cf37d1db60b4a67e4d0d3d7c6596ab8d0
OrangeKyo <sashank.veligati@gmail.com>
2016-08-14 17:11:46 -0500

    Developing basic story structure

--- SashankTestRoom/story.js
+++ SashankTestRoom/story.js
@@ -1,2 +1,39 @@
 "use strict";
 
+var Story = {
+   "createDefaultStory" : function () {
+      var story = Story.createStory();
+      story.pages[0] = Story.createPage("Would you like to write a short story or a long story?");
+      story.pages[0].choices = [
+         Story.createChoice("A short story please!"),
+         Story.createChoice("Are you kidding me? I am pro. I am writing a NOVEL."),
+      ];
+   },
+   "createStory" : function () {
+      return {
+         "pages" : [],
+         "currentPage" : -1,
+         "render" : function () {
+
+         }
+      }
+   },
+   "createPage" : function (text) {
+      if (text === undefined)
+         text = "";
+      
+      return {
+         "text" : "",
+         "choices" : []
+      }
+   },
+   "createChoice" : function (text) {
+      if (text === undefined)
+         text = "";
+      
+      return {
+         "text" : text,
+         "page" : null
+      }
+   }
+}
