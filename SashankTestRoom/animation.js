366c2f88f591c259dd5f4266c0b7acd5bc77b477
OrangeKyo <sashank.veligati@gmail.com>
2016-08-14 00:52:20 -0500

    Associating divs with their gridObjects.

--- SashankTestRoom/animation.js
+++ SashankTestRoom/animation.js
@@ -2,13 +2,8 @@
 var animation = {
    "load" : function () {
       var i = 0, j = 0;
-      document.getElementById("grid").innerHTML = createGridHtml();
-      $("#gridsquare").each(function (index) {
-         var i = index % NUM_COLUMNS;
-         var j = index / NUM_COLUMNS;
-         var gridObject = Grid.createGridObject(i,j);
-         $(this).data(gridObject);
-      });
+      document.getElementById("grid").innerHTML = Grid.createGridHtml();
+      Grid.initializeGrid();
    },
    "run" : function ()
    {
@@ -20,8 +15,50 @@ var NUM_COLUMNS = 50;
 var mGrid = [];
 
 var Grid = {
+   "createGridHtml" : function () {
+      var i, j, result = "";
+      for (i = 0; i < NUM_COLUMNS; i++) {
+         result += "<div>";
+         for (j = 0; j < NUM_ROWS; j++) {
+            result += '<div id="gridsquare" class="square" \
+               onmouseenter="Square.OnEnter($(this));" \
+               onmouseleave="Square.OnLeave($(this));" \
+            ></div>\n';
+         }
+         result += "</div>";
+      }
+      return result;
+   },
+   
+   "initializeGrid" : function () {
+      var i, j;
+      for (i = 0; i < NUM_COLUMNS; i++) {
+         mGrid[i] = [];
+         for (j = 0; j < NUM_ROWS; j++) {
+            mGrid[i][j] = Grid.createGridObject(i,j);
+         }
+      };
+      for (i = 0; i < NUM_COLUMNS; i++) {
+         for (j = 0; j < NUM_ROWS; j++) {
+            var gridObject = Grid.getSquare(i,j);
+            gridObject.top    = Grid.getSquare(i,j-1);
+            gridObject.right  = Grid.getSquare(i+1,j);
+            gridObject.bottom = Grid.getSquare(i,j+1);
+            gridObject.left   = Grid.getSquare(i-1,j);
+         }
+      };
+      $("#gridsquare").each(function (index) {
+         var i = index % NUM_COLUMNS;
+         var j = index / NUM_COLUMNS;
+         var gridObject = Grid.getSquare(i,j);
+         gridObject.div = $(this);
+         $(this).data(gridObject);
+      });
+   },
+   
    "createGridObject" : function (i, j) {
       var gridObject = {
+         "div" : null,
          "i" : i,
          "j" : j,
          "brightness" : 0,
@@ -30,40 +67,45 @@ var Grid = {
          "bottom" : null,
          "left" : null,
       };
-      if (mGrid[i] == null) {
+      if (mGrid[i] == undefined) {
          mGrid[i] = [];
       }
       mGrid[i][j] = gridObject;
       return gridObject;
    },
+   
    "getSquare" : function (i, j) {
-      return mGrid[i][j];
-   }
-};
-
-
-var createGridHtml = function () {
-   var i, j, result = "";
-   for (i = 0; i < NUM_COLUMNS; i++) {
-      result += "<div>";
-      for (j = 0; j < NUM_ROWS; j++) {
-         result += '<div id="gridsquare" class="square" \
-            onmouseenter="Square.OnEnter($(this));" \
-            onmouseleave="Square.OnLeave($(this));" \
-         ></div>\n';
+      if (0 <= i && i < NUM_COLUMNS && 0 <= j && j < NUM_ROWS)
+      {
+         return mGrid[i][j];
       }
-      result += "</div>";
+      return null;
    }
-   return result;
 };
 
 var Square = {
    "OnEnter" : function (div) {
       var gridObject = $(this).data();
-      div.css('background-color', 'blue');
+      gridObject.brightness = 1;
+      if (gridObject.brightness > 0.5)
+      {
+         div.css('background-color', 'blue');
+      }
+      else
+      {
+         div.css('background-color', 'red');
+      }
    },
 
    "OnLeave" : function (div) {
-      div.css('background-color', 'red');
+      var gridObject = $(this).data();
+      if (gridObject.brightness > 0.5)
+      {
+         div.css('background-color', 'blue');
+      }
+      else
+      {
+         div.css('background-color', 'red');
+      }
    },
 };
