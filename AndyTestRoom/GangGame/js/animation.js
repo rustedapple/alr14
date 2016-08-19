1c2cfec67eaed0ba06b277befb07a5f9fdaa6956
Kim <ankim@vvisions.com>
2016-08-19 09:28:41 -0400

    more links

--- AndyTestRoom/GangGame/js/animation.js
+++ AndyTestRoom/GangGame/js/animation.js
@@ -1,19 +1,11 @@
 "use strict";
-var animation = {
-   "load" : function () {
-      var i = 0, j = 0;
-      document.getElementById("grid").innerHTML = Grid.createGridHtml();
-      Grid.initializeGrid();
-   },
-};
-
-// var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { create: create });
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 800, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 var bmd;
 var map;
 var layer;
 var marker;
+
 var currentTile = 0;
 var currentTileMarker;
 var cursors;
@@ -22,32 +14,45 @@ var facing = 'left';
 var jumpTimer = 0;
 var jumpButton;
 
-function preload() {
+const NUM_ROWS = 256;
+const NUM_COLUMNS = 256;
+const BASE_TILEMAP_SIZE = 32;
 
-    //game.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
+function preload() {
 
 }
 
 function create() {
 
-    game.stage.backgroundColor = '#2d2d2d';
+    game.stage.backgroundColor = 'rgba(180, 180, 122, 1)';
+	//game.stage.backgroundColor = "#4488AA";
+    // game.stage.backgroundColor = 0x4488aa;
+    // game.stage.backgroundColor = 'rgb(68, 136, 170)';
+    // game.stage.backgroundColor = 'rgba(68, 136, 170, 0.5)';
 
     //  Creates a blank tilemap
     map = game.add.tilemap();
 
     //  This is our tileset - it's just a BitmapData filled with a selection of randomly colored tiles
     //  but you could generate anything here
-    bmd = game.make.bitmapData(32 * 25, 32 * 2);
-
+    bmd = game.make.bitmapData(BASE_TILEMAP_SIZE * 25, BASE_TILEMAP_SIZE * 2);
+	
+	var graphics = game.add.graphics(BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE);
+	
     var colors = Phaser.Color.HSVColorWheel();
+
 
     var i = 0;
 
     for (var y = 0; y < 2; y++)
     {
         for (var x = 0; x < 25; x++)
-        {
-            bmd.rect(x * 32, y * 32, 32, 32, colors[i].rgba);
+        {//"#996633"
+			
+			// set a fill and line style again
+			//graphics.lineStyle(10, 0xFF0000, 0.8);
+			//graphics.beginFill(0x000000, 1);
+            bmd.rect(x * BASE_TILEMAP_SIZE, y * BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE, '#000000');
             i += 6;
         }
     }
@@ -57,100 +62,45 @@ function create() {
 
     //  Creates a new blank layer and sets the map dimensions.
     //  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
-    layer = map.create('level1', 40, 30, 32, 32);
-
-    //  Populate some tiles for our player to start on
-    map.putTile(30, 2, 10, layer);
-    map.putTile(30, 3, 10, layer);
-    map.putTile(30, 4, 10, layer);
+    layer = map.create('level1', NUM_ROWS, NUM_COLUMNS, BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE);
+	
+	for (var i = 0; i < NUM_COLUMNS; i++) {
+		
+		for (var j = 0; j < NUM_ROWS; j++) {
+			map.putTile(null, i, j, layer);
+			
+			var border;
+			border = game.add.graphics();
+			border.lineStyle(2, 0x000000, 1);
+			border.drawRect(i * NUM_COLUMNS, j * NUM_ROWS, BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE);
+		}
+	}
 
     map.setCollisionByExclusion([0]);
 
     //  Create our tile selector at the top of the screen
     createTileSelector();
 
-    player = game.add.sprite(64, 100, 'dude');
-    game.physics.arcade.enable(player);
-    game.physics.arcade.gravity.y = 350;
-
-    player.body.bounce.y = 0.1;
-    player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
-
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     game.input.addMoveCallback(updateMarker, this);
-
 }
 
 function update() {
-
-    game.physics.arcade.collide(player, layer);
-
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -150;
-
-        if (facing != 'left')
-        {
-            player.animations.play('left');
-            facing = 'left';
-        }
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 150;
-
-        if (facing != 'right')
-        {
-            player.animations.play('right');
-            facing = 'right';
-        }
-    }
-    else
-    {
-        if (facing != 'idle')
-        {
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 5;
-            }
-
-            facing = 'idle';
-        }
-    }
-    
-    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-    {
-        player.body.velocity.y = -250;
-        jumpTimer = game.time.now + 750;
-    }
 
 }
 
 function pickTile(sprite, pointer) {
 
-    var x = game.math.snapToFloor(pointer.x, 32, 0);
-    var y = game.math.snapToFloor(pointer.y, 32, 0);
+    var x = game.math.snapToFloor(pointer.x, BASE_TILEMAP_SIZE, 0);
+    var y = game.math.snapToFloor(pointer.y, BASE_TILEMAP_SIZE, 0);
 
     currentTileMarker.x = x;
     currentTileMarker.y = y;
 
-    x /= 32;
-    y /= 32;
+    x /= BASE_TILEMAP_SIZE;
+    y /= BASE_TILEMAP_SIZE;
 
     currentTile = x + (y * 25);
 
@@ -158,10 +108,10 @@ function pickTile(sprite, pointer) {
 
 function updateMarker() {
 
-    marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
-    marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
+    marker.x = layer.getTileX(game.input.activePointer.worldX) * BASE_TILEMAP_SIZE;
+    marker.y = layer.getTileY(game.input.activePointer.worldY) * BASE_TILEMAP_SIZE;
 
-    if (game.input.mousePointer.isDown && marker.y > 32)
+    if (game.input.mousePointer.isDown && marker.y > BASE_TILEMAP_SIZE)
     {
         map.putTile(currentTile, layer.getTileX(marker.x), layer.getTileY(marker.y), layer);
     }
@@ -173,12 +123,12 @@ function createTileSelector() {
     //  Our tile selection window
     var tileSelector = game.add.group();
 
-    var tileSelectorBackground = game.make.graphics();
-    tileSelectorBackground.beginFill(0x000000, 0.8);
-    tileSelectorBackground.drawRect(0, 0, 800, 66);
-    tileSelectorBackground.endFill();
+    //var tileSelectorBackground = game.make.graphics();
+    //tileSelectorBackground.beginFill(0x000000, 0.8);
+    //tileSelectorBackground.drawRect(0, 0, 800, 66);
+    //tileSelectorBackground.endFill();
 
-    tileSelector.add(tileSelectorBackground);
+    //tileSelector.add(tileSelectorBackground);
 
     var tileStrip = tileSelector.create(1, 1, bmd);
     tileStrip.inputEnabled = true;
@@ -186,17 +136,26 @@ function createTileSelector() {
 
     //  Our painting marker
     marker = game.add.graphics();
-    marker.lineStyle(2, 0x000000, 1);
-    marker.drawRect(0, 0, 32, 32);
+    marker.lineStyle(2, 0xFF0000, 1);
+    marker.drawRect(0, 0, BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE);
 
     //  Our current tile marker
     currentTileMarker = game.add.graphics();
     currentTileMarker.lineStyle(1, 0xffffff, 1);
-    currentTileMarker.drawRect(0, 0, 32, 32);
+    currentTileMarker.drawRect(0, 0, BASE_TILEMAP_SIZE, BASE_TILEMAP_SIZE);
 
     tileSelector.add(currentTileMarker);
 
 }
+/*
+
+var animation = {
+   "load" : function () {
+      var i = 0, j = 0;
+      document.getElementById("grid").innerHTML = Grid.createGridHtml();
+      Grid.initializeGrid();
+   },
+};
 
 const DECAY_RATE = 0.9;
 const BLEED_FACTOR = 0.0252; // Use ~"(1-DECAY_RATE)/4" for good balance.
@@ -531,4 +490,4 @@ var Square = {
       var tile = div.data("tile");
       tile.onLeave();
    },
-};
+};*/
