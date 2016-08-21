84db22d3aa771ca2566637538570df094197217d
Andy Kim <ankim.jin@gmail.com>
2016-08-21 17:08:51 -0400

    committing from android studio test

--- AndyTestRoom/Continuity/android/assets/www/js/index.js
+++ AndyTestRoom/Continuity/android/assets/www/js/index.js
@@ -34,6 +34,14 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+            		window.onload = function() {
+                        // <!-- If you increase the size of the game canvas, you need to also increase the ChunkMap.Map.CHUNK_SIZE constant to match. -->
+                        // <!-- Keep this value a power of 2. -->
+                        var mapSize = 2048;
+                        var continuityEngine = new Continuity.Engine(mapSize, mapSize*3/4, "");
+                    };
+                </script>
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
